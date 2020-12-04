08b4b47d0f3deb60de14a3ea356ed2a36106e611
Jamal .S <me@shrjamal.com>
2020-12-04 22:09:55 +0100

    add chatroom vue

---
+++ src/views/chatroom.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavBar />
+  <div class="container">
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import NavBar from '@/components/navbar.vue'
+export default defineComponent({
+  components: {
+    NavBar,
+  },
+})
+</script>
