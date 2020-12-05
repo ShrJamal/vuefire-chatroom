56cc05edfee387e8a59a38b3d310c27601c07bd9
Jamal .S <me@shrjamal.com>
2020-12-05 21:35:56 +0100

    implemeent chatroom

--- src/views/chatroom.vue
+++ src/views/chatroom.vue
@@ -1,16 +1,54 @@
 <template>
   <NavBar />
   <div class="container">
-    <h1>Welcome</h1>
+    <div class="chat-container" ref="chatContainer">
+      <Message v-for="c in chats" :key="c.id" :msg="c" />
+    </div>
+    <ChatInput @send="sendMessage" />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import { defineComponent, onUnmounted, ref, computed, onUpdated } from 'vue'
 import NavBar from '@/components/navbar.vue'
+import Message from '@/components/chat/message.vue'
+import ChatInput from '@/components/chat/input.vue'
+import { useChatStore } from '@/store/chat'
+
 export default defineComponent({
   components: {
     NavBar,
+    Message,
+    ChatInput,
+  },
+  setup() {
+    const chatContainer = ref()
+    onUpdated(
+      () => (chatContainer.value.scrollTop = chatContainer.value.scrollHeight)
+    )
+    onUnmounted(() => unSub())
+    const store = useChatStore()
+    const unSub = store.chatsStream()
+
+    return {
+      sendMessage: store.sendMessage,
+      chats: computed(() => store.chats),
+      chatContainer,
+    }
   },
 })
 </script>
+
+<style scoped>
+.container {
+  max-height: 600px;
+  display: flex;
+  flex-direction: column;
+}
+
+.chat-container {
+  margin-left: 20px;
+  overflow: auto;
+  flex: 1;
+}
+</style>
