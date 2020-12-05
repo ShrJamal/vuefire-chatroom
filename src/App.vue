2cc587b757fef9506c9d774b6e0bc779032ee9a8
Jamal .S <me@shrjamal.com>
2020-12-05 21:36:42 +0100

    refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
+  <h1 v-show="!store.authInit">Loading...</h1>
   <router-view v-show="store.authInit" />
 </template>
 
 <script lang="ts">
 import { defineComponent, onUnmounted } from 'vue'
 import { auth } from '@/utils/firebase'
-import type { Unsubscribe } from '@/utils/firebase.ts'
 import { useAuthStore } from '@/store/auth/index'
 import { useRouter } from 'vue-router'
 
 export default defineComponent({
   setup() {
-    let unSubscribe: Unsubscribe
     onUnmounted(() => unSubscribe())
 
     const router = useRouter()
     const store = useAuthStore()
-    unSubscribe = auth.onAuthStateChanged(async () => {
+    const unSubscribe = auth.onAuthStateChanged(async () => {
       console.log('----onAuthStateChanged ')
       if (await store.fetchUser()) {
         router.push({ name: 'Auth' })
